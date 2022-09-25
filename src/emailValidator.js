0634a3250c2b99a32b0932fef32e8550774c20be
Tristan <tristanmayofreire@gmail.com>
2022-09-25 19:38:20 +0200

    deleted old notes

--- src/emailValidator.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-$url = 'https://emailverification.whoisxmlapi.com/api/v1?apiKey=&emailAddress='. $email;
-return file_get_contents($url);
-
-if($emailValidation['dnsCheck'] == 'false' || $emailValidation['disposableCheck'] == 'true') 
-{
-  echo $heikeApiWrapper->sendError('El email no es valido. Por favor prueba con otro email');
