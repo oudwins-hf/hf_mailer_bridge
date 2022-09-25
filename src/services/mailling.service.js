2c5a90acc82df7f905f2ec709549ecf67da266b0
Tristan <tristanmayofreire@gmail.com>
2022-09-25 19:48:44 +0200

    created mailing service (facade for node-mailwizz)

---
+++ src/services/mailling.service.js
@@ -0,0 +1,43 @@
+const mailwizz = require("node-mailwizz");
+const config = require("../config/config");
+
+const subscribers = new mailwizz.ListSubscribers({
+  publicKey: config.mailwizz.publicKey,
+  secret: config.mailwizz.secret,
+  baseUrl: config.mailwizz.url,
+});
+
+const searchByEmail = async function (listId, email) {
+  return new Promise((resolve, reject) => {
+    subscribers
+      .emailSearch(listId, email)
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
+  });
+};
+
+const update = async function (listId, userId, userInfo) {
+  return new Promise((resolve, reject) => {
+    subscribers
+      .update(listId, userId, userInfo)
+      .then((result) => resolve(result))
+      .catch((err) => reject(err));
+  });
+};
+
+const create = async function (listId, userInfo) {
+  return new Promise((resolve, reject) => {
+    subscribers
+      .create(listId, userInfo)
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
+  });
+};
+
+module.exports = {
+  subscribers: {
+    searchByEmail,
+    update,
+    create,
+  },
+};
